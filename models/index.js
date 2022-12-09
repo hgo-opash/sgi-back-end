5bb870fa6a2633096c02a3c8f3dc1de3a9c6f855
Hitesh <hgo.opash@gmail.com>
2022-12-09 13:05:07 +0530

    New Change

--- models/index.js
+++ models/index.js
@@ -1,9 +1,7 @@
-const dbConfig = require("../config/db.config");
 const mongoose = require("mongoose");
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
 // db.users = require("./users.model")(mongoose);
 db.users = require("./users.model");
 db.suscriptions = require("./subscriptions.model");
